65f76096d8a3fa9c9cb3df245c7cff80e2a16a80
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-06 07:23:43 -0300

    Esc key to close popup and alert error

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from './modal.js';
+import { AlertError } from './alert-error.js';
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -12,6 +13,15 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if(showAlertError) {
+    AlertError.open()
+    return
+  }
+  
+  AlertError.close()
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
@@ -19,7 +29,9 @@ form.onsubmit = event => {
   Modal.open()
 }
 
-
+function notANumber(value) {
+  return isNaN(value) || value == ""
+}
 
 
 function IMC(weight, height) {
